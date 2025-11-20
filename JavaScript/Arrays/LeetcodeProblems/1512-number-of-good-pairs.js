1b7831aa151a1b1bf794ed1db2dc9dca48196d59
navneetsharma-cs21 <ivaann025@gmail.com>
2025-11-20 13:42:02 +0530

    Added solution for LeetCode 1512: Number of Good Pairs

---
+++ JavaScript/Arrays/LeetcodeProblems/1512-number-of-good-pairs.js
@@ -0,0 +1,20 @@
+// 1512. Number of Good Pairs
+// Problem: Count pairs (i, j) where nums[i] == nums[j] and i < j.
+
+var numIdenticalPairs = function(nums) {
+    let count = 0
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i]==nums[j]){
+                count++;
+            }
+       
+        }
+    
+    }
+    return count;
+};
+
+console.log(numIdenticalPairs([1,2,3,1,1,3])); 
+console.log(numIdenticalPairs([1,1,1,1]));     
+console.log(numIdenticalPairs([1,2,3]));  
