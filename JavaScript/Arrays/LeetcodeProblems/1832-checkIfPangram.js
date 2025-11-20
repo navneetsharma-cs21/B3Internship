03fb4456ee478c00a57c0211497bcbceee0d6f25
navneetsharma-cs21 <ivaann025@gmail.com>
2025-11-20 15:15:11 +0530

    Added solution for LeetCode 1832: Check if the Sentence Is Pangram

---
+++ JavaScript/Arrays/LeetcodeProblems/1832-checkIfPangram.js
@@ -0,0 +1,17 @@
+// 1832. Check if the Sentence Is Pangram
+// Problem: Return true if sentence contains every letter a-z at least once.
+
+//  let set = new Set(sentence);
+// - In JavaScript, a Set is a collection of unique values.
+// - When you pass a string (like sentence) into the Set constructor, JavaScript treats the string as an iterable — meaning it will break it down into individual characters.
+// - So new Set(sentence) creates a set containing all distinct characters from the string.
+
+
+var checkIfPangram = function(sentence) {
+    let set = new Set(sentence)
+        return set.size == 26;
+};
+
+console.log(checkIfPangram("thequickbrownfoxjumpsoverthelazydog"));
+console.log(checkIfPangram("leetcode"));
+console.log(checkIfPangram("abcdefghijklmnopqrstuvwxyz"));
