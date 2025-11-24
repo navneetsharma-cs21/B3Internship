44c2a631bdec81dd77c6d0769dda52f5f4633f62
navneetsharma-cs21 <ivaann025@gmail.com>
2025-11-24 09:57:50 +0530

    Added a js project Calculator

---
+++ WebPages/JavaScriptProjects/Calculator/script.js
@@ -0,0 +1,6 @@
+let num = parseInt(prompt("enter number 1: "));
+let num2 = parseInt(prompt("enter number 2 "));
+
+let sum  = num+num2;
+
+console.log(sum);
---
+++ WebPages/Reactjs/my-app1/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+function App() {
+  
+  return (
+    <>
+    <h1>Navneet Sharma</h1>
+    <h2>hii</h2>
+    </>
+  )
+}
+
+export default App
